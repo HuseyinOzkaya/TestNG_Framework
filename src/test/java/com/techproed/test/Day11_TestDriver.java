275cea7d93aed331dbf986dd3e19af21487f391e
unknown <ozkayahuseyin1@gmail.com>
2021-10-12 21:54:45 -0400

    huseyin2

---
+++ src/test/java/com/techproed/test/Day11_TestDriver.java
@@ -0,0 +1,25 @@
+package com.techproed.test;
+
+import com.techproed.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day11_TestDriver {
+
+    //Create FirstDriverTest
+    //Check if Driver class is working
+    // Go to amazon page
+    //Verify the title includes Amazon
+    //Check if Driver class is working
+    //https://www.amazon.ca/
+
+    @Test
+    public void FirstDriverTest(){
+        Driver.getDriver().get("https://www.amazon.ca/");
+      String title =  Driver.getDriver().getTitle();
+        Assert.assertTrue(title.contains("Amazon"));
+        Driver.closeDriver();
+    }
+
+}
